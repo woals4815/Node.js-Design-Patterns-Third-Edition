9fcdc54060fe01b8c3f666a099a1f3fc33bc47e6
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-08-04 17:03:50 +0900

    Add some comments in app.js and edit package json

--- 12-scalability-and-architectural-patterns/03-http-cluster-resilient/app.js
+++ 12-scalability-and-architectural-patterns/03-http-cluster-resilient/app.js
@@ -1,28 +1,34 @@
-import { createServer } from 'http'
-import { cpus } from 'os'
-import cluster from 'cluster'
+import { createServer } from "http";
+import { cpus } from "os";
+import cluster from "cluster";
 
 if (cluster.isMaster) {
-  const availableCpus = cpus()
-  console.log(`Clustering to ${availableCpus.length} processes`)
-  availableCpus.forEach(() => cluster.fork())
-  cluster.on('exit', (worker, code) => {
+  const availableCpus = cpus();
+  console.log(`Clustering to ${availableCpus.length} processes`);
+  availableCpus.forEach(() => cluster.fork());
+  //exit 이벤트 수신
+  cluster.on("exit", (worker, code) => {
+    //의도적으로 종료됐는지 오류로 인해 종료됐는지 확인
     if (code !== 0 && !worker.exitedAfterDisconnect) {
-      console.log(`Worker ${worker.process.pid} crashed. Starting a new worker`)
-      cluster.fork()
+      console.log(
+        `Worker ${worker.process.pid} crashed. Starting a new worker`
+      );
+      cluster.fork();
     }
-  })
+  });
 } else {
-  setTimeout(
-    () => { throw new Error('Ooops') },
-    Math.ceil(Math.random() * 3) * 1000
-  )
-  const { pid } = process
+  setTimeout(() => {
+    throw new Error("Ooops");
+  }, Math.ceil(Math.random() * 3) * 1000);
+  const { pid } = process;
   const server = createServer((req, res) => {
-    let i = 1e7; while (i > 0) { i-- }
-    console.log(`Handling request from ${pid}`)
-    res.end(`Hello from ${pid}\n`)
-  })
+    let i = 1e7;
+    while (i > 0) {
+      i--;
+    }
+    console.log(`Handling request from ${pid}`);
+    res.end(`Hello from ${pid}\n`);
+  });
 
-  server.listen(8080, () => console.log(`Started at ${pid}`))
+  server.listen(8080, () => console.log(`Started at ${pid}`));
 }
