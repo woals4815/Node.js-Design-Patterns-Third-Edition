df18a7d952f9f7245d63b76317ce370ee971feea
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-08-05 10:47:06 +0900

    Add comments and fix package.json

--- 12-scalability-and-architectural-patterns/06-http-dynamic-load-balancer/app.js
+++ 12-scalability-and-architectural-patterns/06-http-dynamic-load-balancer/app.js
@@ -1,55 +1,65 @@
-import { createServer } from 'http'
-import consul from 'consul'
-import portfinder from 'portfinder'
-import { nanoid } from 'nanoid'
+import { createServer } from "http";
+import consul from "consul";
+import portfinder from "portfinder";
+import { nanoid } from "nanoid";
 
-const serviceType = process.argv[2]
-const { pid } = process
+const serviceType = process.argv[2];
+const { pid } = process;
 
-async function main () {
-  const consulClient = consul()
+async function main() {
+  const consulClient = consul();
 
-  const port = await portfinder.getPortPromise() // ①
-  const address = process.env.ADDRESS || 'localhost'
-  const serviceId = nanoid()
+  const port = await portfinder.getPortPromise(); // ① 이걸 사용해 시스템에서 사용가능한 포트를 찾는다. 기본적으로 8000에서 검색 시작
+  const address = process.env.ADDRESS || "localhost";
+  const serviceId = nanoid();
 
-  function registerService () { // ②
-    consulClient.agent.service.register({
-      id: serviceId,
-      name: serviceType,
-      address,
-      port,
-      tags: [serviceType]
-    }, () => {
-      console.log(`${serviceType} registered successfully`)
-    })
+  function registerService() {
+    // ② 레지스트리에 새 서비스를 등록하기 위해 저 함수 선언.
+    consulClient.agent.service.register(
+      {
+        id: serviceId,
+        name: serviceType,
+        address,
+        port,
+        tags: [serviceType],
+      },
+      () => {
+        console.log(`${serviceType} registered successfully`);
+      }
+    );
   }
 
-  function unregisterService (err) { // ③
-    err && console.error(err)
-    console.log(`deregistering ${serviceId}`)
+  function unregisterService(err) {
+    // ③ 방금 등록한 서비스를 제거하 ㄹ수 있는 함수 선언
+    err && console.error(err);
+    console.log(`deregistering ${serviceId}`);
     consulClient.agent.service.deregister(serviceId, () => {
-      process.exit(err ? 1 : 0)
-    })
+      process.exit(err ? 1 : 0);
+    });
   }
 
-  process.on('exit', unregisterService) // ④
-  process.on('uncaughtException', unregisterService)
-  process.on('SIGINT', unregisterService)
+  process.on("exit", unregisterService); // ④ 종료 됐을 때 서비스가 컨설에서 등록 해제
+  process.on("uncaughtException", unregisterService);
+  process.on("SIGINT", unregisterService);
 
-  const server = createServer((req, res) => { // ⑤
-    let i = 1e7; while (i > 0) { i-- }
-    console.log(`Handling request from ${pid}`)
-    res.end(`${serviceType} response from ${pid}\n`)
-  })
+  const server = createServer((req, res) => {
+    // ⑤ 포트 파인더에서 발견한 포트와 현재 서비스에 대해 설정된 주소에서 서비스용 http 서버 시작. 시작되면 함수 호출 후
+    //서비스 검색을 위해 등록
+    let i = 1e7;
+    while (i > 0) {
+      i--;
+    }
+    console.log(`Handling request from ${pid}`);
+    res.end(`${serviceType} response from ${pid}\n`);
+  });
 
   server.listen(port, address, () => {
-    registerService()
-    console.log(`Started ${serviceType} at ${pid} on port ${port}`)
-  })
+    registerService();
+    console.log(`Started ${serviceType} at ${pid} on port ${port}`);
+  });
 }
 
 main().catch((err) => {
-  console.error(err)
-  process.exit(1)
-})
+  console.error(err);
+  process.exit(1);
+});
--- 12-scalability-and-architectural-patterns/06-http-dynamic-load-balancer/loadBalancer.js
+++ 12-scalability-and-architectural-patterns/06-http-dynamic-load-balancer/loadBalancer.js
@@ -1,39 +1,47 @@
-import { createServer } from 'http'
-import httpProxy from 'http-proxy'
-import consul from 'consul'
+import { createServer } from "http";
+import httpProxy from "http-proxy";
+import consul from "consul";
 
-const routing = [ // ①
+const routing = [
+  // ① 먼저 로드 밸런서 경로를 정의. 각 항목에는 맵핑된 경로에 도착하는 요청을 처리하는데 사용되는 서비스 포함. 인덱스는 서비스 요청을 라운드로빈하는데 사용
   {
-    path: '/api',
-    service: 'api-service',
-    index: 0
+    path: "/api",
+    service: "api-service",
+    index: 0,
   },
   {
-    path: '/',
-    service: 'webapp-service',
-    index: 0
-  }
-]
+    path: "/",
+    service: "webapp-service",
+    index: 0,
+  },
+];
 
-const consulClient = consul() // ②
-const proxy = httpProxy.createProxyServer()
+const consulClient = consul(); // ② 레지스트리에 액세스할 수 있도록 컨설 클라이언트를 인스턴스화. 다음으로 http-proxy 서버를 인스턴스화
+const proxy = httpProxy.createProxyServer();
 
 const server = createServer((req, res) => {
-  const route = routing.find((route) => req.url.startsWith(route.path)) // ③
-  consulClient.agent.service.list((err, services) => { // ④
-    const servers = !err && Object.values(services)
-      .filter(service => service.Tags.includes(route.service))
+  const route = routing.find((route) => req.url.startsWith(route.path)); // ③ 서버의 요청 핸들러에서 먼저 하는 것은 라우팅 테이블에서 URL 찾기.
+  //결과는 서비스 이름이 포함된 설명자
+  consulClient.agent.service.list((err, services) => {
+    // ④ 컨설로부터 요청 처리하는데 필요한 서비스를 구현한 서버의 목록을 받음. 비어있거나 오류는 클라에 오류 반환
+    //tags 속성을 사용해 사용가능하 모든 서비스를 필터링하고 현재 서비스 유형을 구현하고 있는 서버의 주소를 찾는다
+    const servers =
+      !err &&
+      Object.values(services).filter((service) =>
+        service.Tags.includes(route.service)
+      );
 
     if (err || !servers.length) {
-      res.writeHead(502)
-      return res.end('Bad gateway')
+      res.writeHead(502);
+      return res.end("Bad gateway");
     }
 
-    route.index = (route.index + 1) % servers.length // ⑤
-    const server = servers[route.index]
-    const target = `http://${server.Address}:${server.Port}`
-    proxy.web(req, res, { target })
-  })
-})
+    route.index = (route.index + 1) % servers.length; // ⑤ 요청을 대상으로 라우팅. 라운드로빈 방식에 다라 목록에서 다음 서버를 가리키토도록 인덱스 갱신
+    // 그 다음 인덱스를 사용해 목록에서 서버를 선택하고 요청 및 응답 객체랑 같이 proxy.web()으로 전달
+    const server = servers[route.index];
+    const target = `http://${server.Address}:${server.Port}`;
+    proxy.web(req, res, { target });
+  });
+});
 
-server.listen(8080, () => console.log('Load balancer started on port 8080'))
+server.listen(8080, () => console.log("Load balancer started on port 8080"));
